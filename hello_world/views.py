a7a5547dd6cb0fa5246edc333217e0e2b10c2604
Nigel Finegan <fvunpiqh@students.codeinstitute.net>
2024-11-12 11:39:34 +0000

    file update urls.py & views.py

--- hello_world/views.py
+++ hello_world/views.py
@@ -5,4 +5,10 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world!")
 
+
+# Create your views here.
+def about_me(request):
+    return HttpResponse("This would be the about page")
+
+
     
